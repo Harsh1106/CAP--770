b3b16d9f745b7e046e378887d14c12580791b769
harsh <sinha.harshraj1101@gmail.com>
2024-05-08 23:27:55 +0530

    addded bfs

---
+++ bfs.cpp
@@ -0,0 +1,44 @@
+// BFS: Breadth First Search
+// It is just a traversal technique
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+
+void BFS(vector<vector<int>> &adjList, int start, vector<bool> &visited){
+    queue<int> q;
+
+    visited[start] = true;
+    q.push(start);
+    while(!q.empty()){
+        int curr = q.front();
+        q.pop();
+        cout<<curr<<" ";
+        for(int neighbour : adjList[curr]){
+            if(!visited[neighbour]){
+                visited[neighbour] = true;
+                q.push(neighbour);
+            }
+        }
+    }
+}
+
+void addEdge(vector<vector<int>> &adjList, int u, int v){
+    adjList[u].push_back(v);
+}
+int main(){
+    int vertices = 5;
+    vector<vector<int>> adjList(vertices);
+    addEdge(adjList, 0, 1);
+    addEdge(adjList, 0, 2);
+    addEdge(adjList, 1, 3);
+    addEdge(adjList, 1, 4);
+    addEdge(adjList, 2, 4);
+    vector<bool> visited(vertices, false);
+    cout<<"Breadth first search traversal starting from index 0: "<<endl;
+    BFS(adjList, 0, visited);
+
+
+    return 0;
+}
