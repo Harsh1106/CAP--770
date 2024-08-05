85d0206c96253a837d671e2d7efa352ac0cc2094
harsh <sinha.harshraj1101@gmail.com>
2024-08-05 11:38:59 +0530

    Added code of Insertion sort

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -4,16 +4,12 @@ void insertionSort(int arr[], int size){
     for(int i = 1 ; i < size ; i++)
     {
         int temp = arr[i];
-       
-        for( int j = i-1 ; j >= 0 ; j--)
-        {
-            if(arr[j] > temp){
-                arr[j+1] = arr[j];
-            }else{
-                break;
-            }
-            arr[j+1] = temp;
+        int j = i - 1;
+        while(j >= 0 && arr[j] > temp){
+            arr[j + 1] = arr[j];
+            j--;
         }
+        arr[j + 1] = temp;
         
     }
 }
